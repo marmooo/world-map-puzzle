686dae585e5b07c179fe5090b1e21635b7707dfc
marmooo <marmooo@users.noreply.github.com>
2024-02-20 00:50:50 +0900

    use npx

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p docs
 cp -r src/* docs
-svgo src/map.svg --precision 0 -o docs/map.svg
+npx svgo src/map.svg --precision 0 -o docs/map.svg
 drop-inline-css -r src -o docs
 minify -r docs -o .
