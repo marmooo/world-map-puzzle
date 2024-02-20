6a6460839e2995a9502988ed2bdb493c29dcb11f
marmooo <marmooo@users.noreply.github.com>
2024-02-20 09:43:10 +0900

    bump svgo 3.2.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p docs
 cp -r src/* docs
-npx svgo src/map.svg --precision 0 -o docs/map.svg
+npx svgo@3.2.0 src/map.svg --precision 0 -o docs/map.svg
 drop-inline-css -r src -o docs
 minify -r docs -o .
