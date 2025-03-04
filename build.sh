0583ddeda4cdfed7a94f8c8d176f5c2ada71b5d8
marmooo <marmooo@users.noreply.github.com>
2025-03-05 04:29:14 +0900

    patch minify bug

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p docs
 cp -r src/* docs
 npx svgo@3.3.2 src/map.svg --precision 0 -o docs/map.svg
-deno run -A bundle.js ./src/index.js > docs/index.js
 drop-inline-css -r src -o docs
 minify -r docs -o .
+deno run -A bundle.js ./src/index.js > docs/index.js
--- bundle.js
+++ bundle.js
@@ -1,5 +1,15 @@
 import { bundle } from "jsr:@deno/emit";
+import { minify } from "npm:@swc/core";
 
+// const url = new URL(import.meta.resolve(Deno.args[0]));
+// const { code } = await bundle(url);
+// console.log(code);
 const url = new URL(import.meta.resolve(Deno.args[0]));
 const { code } = await bundle(url);
-console.log(code);
+const minified = await minify(code, {
+  module: true,
+  compress: true,
+  mangle: true,
+  sourceMap: false,
+});
+console.log(minified.code);
