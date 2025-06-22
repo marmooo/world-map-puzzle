ad71cdfc4f37b3f63f089c0a14f0bfcc21d3d083
marmooo <marmooo@users.noreply.github.com>
2025-06-23 08:58:54 +0900

    bump fabric.js from 6.6.5 to 6.7.0

--- src/en/sw.js
+++ src/en/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2025-06-22 03:20";
+const CACHE_NAME = "2025-06-23 00:00";
 const urlsToCache = [
   "/world-map-puzzle/",
   "/world-map-puzzle/en/",
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   Rect,
   Text,
   util,
-} from "https://cdn.jsdelivr.net/npm/fabric@6.6.5/+esm";
+} from "https://cdn.jsdelivr.net/npm/fabric@6.7.0/+esm";
 import svgpath from "https://cdn.jsdelivr.net/npm/svgpath@2.6.0/+esm";
 
 const htmlLang = document.documentElement.lang;
