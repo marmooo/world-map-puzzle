d33b7b5f94950b0c7cbfd209e4763a752c2590a6
marmooo <marmooo@users.noreply.github.com>
2025-10-19 16:09:58 +0900

    use deno bundle

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cp -r src/* docs
 npx svgo@4.0.0 src/map.svg --precision 0 -o docs/map.svg
 drop-inline-css -r src -o docs
 minify -r docs -o .
-deno run -A bundle.js ./src/index.js > docs/index.js
+deno bundle src/index.js -o docs/index.js
