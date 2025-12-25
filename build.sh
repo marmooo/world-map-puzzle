7a9bd7351398823e93f3fa1a6a1bddfa1ca95b58
marmooo <marmooo@users.noreply.github.com>
2025-12-26 00:43:03 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mkdir -p docs
 cp -r src/* docs
 npx svgo@4.0.0 src/map.svg --precision 0 -o docs/map.svg
 drop-inline-css -r src -o docs
-minify -r docs -o .
 deno bundle src/index.js -o docs/index.js
+minify -r docs -o .
