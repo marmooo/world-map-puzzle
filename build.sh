2ccb748419812077a6234793821c90f06f3e1ef8
marmooo <marmooo@users.noreply.github.com>
2026-01-29 10:32:00 +0900

    fix minify bug

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mkdir -p docs
 cp -r src/* docs
 npx svgo@4.0.0 src/map.svg --precision 0 -o docs/map.svg
 drop-inline-css -r src -o docs
-deno run -RWE --allow-ffi bundle.js ./src/index.js > ./docs/index.js
 minify -r docs -o .
+deno run -RWE --allow-ffi bundle.js ./src/index.js > ./docs/index.js
